b3572f356f466a727acd1e91004155de97371a3f
cbrookscode <colbybrooks@myyahoo.com>
2024-07-02 16:13:22 -0400

    draw move cell meth implemented

--- src/cell.py
+++ src/cell.py
@@ -1,4 +1,4 @@
-from graphics import Point, Line
+from graphics import Point, Line, Window
 
 class Cell:
     def __init__(self, point1, point2, window, left_wall=True, right_wall=True, top_wall=True, bottom_wall=True):
@@ -21,3 +21,11 @@ class Cell:
             Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(self._win.canvas, "red")
         if self.has_bottom_wall == True:
             Line(Point(self._x2, self._y2), Point(self._x1, self._y2)).draw(self._win.canvas, "red")
+    
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo == True:
+            color = "gray"
+        point1 = Point(((self._x2 + self._x1) / 2), ((self._y2 + self._y1) / 2))
+        point2 = Point(((to_cell._x2 + to_cell._x1) / 2), ((to_cell._y2 + to_cell._y1) / 2))
+        Line(point1, point2).draw(self._win.canvas, color)
