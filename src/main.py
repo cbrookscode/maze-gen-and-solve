6616359f9a6bcb4538281d34014e1ddf66446662
cbrookscode <colbybrooks@myyahoo.com>
2024-07-03 12:26:19 -0400

    added maze.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 from cell import Cell
 from graphics import Window, Point
+from maze import *
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(Point(200, 200), Point(500, 500), win, bottom_wall=False)
-    cell2 = Cell(Point(250, 250), Point(550, 550), win)
-    cell3 = Cell(Point(300, 300), Point(500, 500), win, top_wall=False)
-    cell4 = Cell(Point(350, 350), Point(500, 500), win, right_wall=False)
-
-    cell2.draw_move(cell1)
+    maze = Maze(20, 20, 10, 10, 5, 5, win)
+    maze._draw_cell()
 
     win.wait_for_close()
 
