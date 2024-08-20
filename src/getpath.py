e4abd40c69f337b6a2861cc9fcf70bc989b77e39
cbrookscode <colbybrooks@myyahoo.com>
2024-08-20 13:38:39 -0400

    update

---
+++ src/getpath.py
@@ -0,0 +1,7 @@
+import os
+
+file_path = '~/workspace/github.com/cbrookscode/mazesolver/maze-gen-and-solve/src/cell.py'
+
+absolute_path = os.path.abspath(file_path)\
+
+print("Absolutel path of the file: ", absolute_path)
