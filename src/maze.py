9f5bad276c7110d5b28eede458a5e90d0498b594
cbrookscode <colbybrooks@myyahoo.com>
2025-02-07 11:25:45 -0500

    finished maze logic for create draw and animate

--- src/maze.py
+++ src/maze.py
@@ -11,26 +11,21 @@ class Maze:
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
-        self._cells = []
+        self._cells = None
 
         self._create_cells(num_rows, num_cols)
 
     def _create_cells(self, rows, cols):
-        tracker = []
-        for i in range(rows):
-            for j in range(cols):
-                tracker.append(Cell(self.win))
-            self._cells.append(tracker)
-            tracker = []
+        self._cells = [[Cell(self.win) for col in range(cols)] for row in range(rows)]
         self._draw_cell()
 
     def _draw_cell(self):
         x = self.x1
         y = self.y1
         for column in self._cells:
-            point1 = Point(x, y)
-            point2 = Point(x + self.cell_size_x, y + self.cell_size_y)
             for cell in column:
+                point1 = Point(x, y)
+                point2 = Point(x + self.cell_size_x, y + self.cell_size_y)
                 cell.draw(point1, point2)
                 self._animate()
                 y += self.cell_size_y
@@ -40,4 +35,4 @@ class Maze:
 
     def _animate(self):
         self.win.redraw()
-        time.sleep(0.05)
+        time.sleep(0.15)
