6507e1b2602e89457219cf5c184c07768810b43a
cbrookscode <colbybrooks@myyahoo.com>
2025-02-12 14:45:25 -0500

    break walls function finished

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from src.maze import *
 
 win = Window(800, 600)
 
-mytestmaze = Maze(15, 15, 3, 3, 50, 50, win)
+mytestmaze = Maze(15, 15, 3, 3, 50, 50, win, 2)
 
 win.wait_for_close()
--- src/maze.py
+++ src/maze.py
@@ -9,8 +9,6 @@ class Maze:
             raise Exception("Maze cannot have 0 rows or columns when created")
         if num_rows < 0 or num_cols < 0:
             raise Exception("Maze cannot a negative value for either rows or columns when created")
-        if seed is not None:
-            random.seed(seed)
         self.x1 = x1
         self.y1 = y1
         self.num_rows = num_rows
@@ -19,6 +17,9 @@ class Maze:
         self.cell_size_y = cell_size_y
         self.win = win
         self._cells = None
+        self.seed = None
+        if seed is not None:
+            self.seed = random.seed(seed)
 
         self._create_cells(num_rows, num_cols)
 
@@ -60,69 +61,54 @@ class Maze:
         exit.draw(Point(exit.x1, exit.y1), Point(exit.x2, exit.y2))
         self._animate()
     
-    def is_valid_cell(self, i, j):
-        return (i >= 0 and 
-                i < len(self._cells) and 
-                j >= 0 and 
-                j < len(self._cells[0]))
+    def is_valid_cell(self, i_j_tuple):
+        return (i_j_tuple[0] >= 0 and 
+                i_j_tuple[0] < len(self._cells) and 
+                i_j_tuple[1] >= 0 and 
+                i_j_tuple[1] < len(self._cells[0]))
     
     def _break_walls_r(self, i , j):
         current_cell = self._cells[i][j]
         current_cell.visited = True
         while True:
-            visited = []
             not_visited =[]
-            # how to check adjacent cells? check all four sides. how to check a side. top and bottom easy as they are the before and after part of the list. left and right would be the same index but the before and after list.
-            if self.is_valid_cell(i - 1, j) == True:
-                if self._cells[i - 1][j].visited == False:
-                    not_visited.append(self._cells[i - 1][j])
-                else:
-                    visited.append(self._cells[i - 1][j])
+            left = (i - 1, j)
+            right = (i + 1 , j)
+            top = (i, j - 1)
+            bottom = (i, j + 1)
+            if self.is_valid_cell(left) == True and self._cells[left[0]][left[1]].visited == False:
+                not_visited.append(left)
 
-            if self.is_valid_cell(i + 1, j) == True:
-                if self._cells[i + 1][j].visited == False:
-                    not_visited.append(self._cells[i + 1][j])
-                else:
-                    visited.append(self._cells[i + 1][j])
+            if self.is_valid_cell(right) == True and self._cells[right[0]][right[1]].visited == False:
+                not_visited.append(right)
 
-            if self.is_valid_cell(i, j - 1) == True:
-                if self._cells[i][j - 1].visited == False:
-                    not_visited.append(self._cells[i][j - 1])
-                else:
-                    visited.append(self._cells[i][j - 1])
+            if self.is_valid_cell(top) == True and self._cells[top[0]][top[1]].visited == False:
+                not_visited.append(top)
 
-            if self.is_valid_cell(i, j + 1) == True:
-                if self._cells[i][j + 1].visited == False:
-                    not_visited.append(self._cells[i][j + 1])
-                else:
-                    visited.append(self._cells[i][j + 1])
+            if self.is_valid_cell(bottom) == True and self._cells[bottom[0]][bottom[1]].visited == False:
+                not_visited.append(bottom)
 
-            if len(visited) == 0:
+            if len(not_visited) == 0:
                 current_cell.draw(Point(current_cell.x1, current_cell.y1), Point(current_cell.x2, current_cell.y2))
                 self._animate()
                 return
             else:
-                random_index = random.randrange(0, len(not_visited))
-                new_cell = not_visited[random_index]
-                new_cell.visited = True
-                # with the x and y points for the new cell and old cell, how to determine which wall they share?
-                if new_cell.x1 == current_cell.x1 and new_cell.x2 == current_cell.x2:
-                    #share a top or bottom line
-                    if Point(new_cell.x1, new_cell.y1) == Point(current_cell.x1, current_cell.y2):
-                        #share a bottom wall
-                        current_cell.has_bottom_wall = False
-                        self._break_walls_r(i, j + 1)
-                    else:
-                        #share a top wall
-                        current_cell.has_top_wall = False
-                        self._break_walls_r(i, j - 1)
-                if new_cell.y1 == current_cell.y1 and new_cell.y2 == current_cell.y2:
-                    #share a left or right line
-                    if Point(new_cell.x1, new_cell.y1) == Point(current_cell.x2, current_cell.y1):
-                        #share a right wall
-                        current_cell.has_right_wall = False
-                        self._break_walls_r(i + 1, j)
-                    else:
-                        #share a left wall
-                        current_cell.has_left_wall = False
-                        self._break_walls_r(i - 1, j)
+                random_index = None
+                if self.seed != None:
+                    random_index = self.seed
+                else:
+                    random_index = random.randrange(0, len(not_visited))
+                new_i = not_visited[random_index][0]
+                new_j = not_visited[random_index][1]
+                if new_i == i and new_j == j + 1:
+                    current_cell.has_bottom_wall = False
+                    self._break_walls_r(new_i, new_j)
+                if new_i == i and new_j == j - 1:
+                    current_cell.has_top_wall = False
+                    self._break_walls_r(new_i, new_j)
+                if new_i == i + 1 and new_j == j:
+                    current_cell.has_right_wall = False
+                    self._break_walls_r(new_i, new_j)
+                if new_i == i - 1 and new_j == j:
+                    current_cell.has_left_wall = False
+                    self._break_walls_r(new_i, new_j)
